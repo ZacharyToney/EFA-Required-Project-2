d2990d7484e7389edd3b63d9074bc0e835b6b799
ZacharyToney <zach@clearseries.com>
2016-07-26 10:31:45 -0400

    Finished MVC Project

--- MvcMovie/App_Start/RouteConfig.cs
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -17,6 +17,11 @@ namespace MvcMovie
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
+            );
+            routes.MapRoute(
+            name: "Hello",
+            url: "{controller}/{action}/{name}/{id}"
             );
         }
     }
